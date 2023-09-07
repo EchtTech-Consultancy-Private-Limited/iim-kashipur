e2f8b8f8987aae423487634ef9e425e390b87306
ET-GS <gireesh.shukla@echttech.com>
2023-09-07 10:32:20 +0530

    401 page changes

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -60,7 +60,7 @@ h1, a{
 @section('content')
 <div class="section">
     <h1 class="error">404</h1>
-    <div class="page">Ooops!!! The page you are looking for is not found</div>
+    <div class="page">The page you are looking for is not found</div>
     <a class="back-home" href="{{ url('/') }}">Back to home</a>
   </div>
 
