a711c572dd887bb38a99e5ebdc4ef6cbff3c42df
your_username <vishal.singh@echttech.com>
2023-11-23 14:10:47 +0530

    Create tdxe page

--- resources/views/front/Layouts/child_pages/menu_bar/main_menu/tdex.blade.php
+++ resources/views/front/Layouts/child_pages/menu_bar/main_menu/tdex.blade.php
@@ -2,9 +2,273 @@
 
 @section('content')
 
+<div class="internalpagebanner">
+   <img src="{{ asset('page/banner/1700541845Group_Photo_TEDX1.jpg') }}" alt="" title="" loading="lazy">            
+   <div class="imagecaption">
+      <div class="container">
+         <h1 tabindex="0">TEDx</h1>
+      </div>
+   </div>
+</div>
+
+<div class="breadcrumbs">
+   <div class="container">
+      <ul>
+         <li>
+            <a href="https://www.iimkashipur.ac.in">
+               <svg viewBox="0 0 24 24">
+                  <path fill="none" d="M0 0h24v24H0V0z"></path>
+                  <path d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"></path>
+               </svg>
+            </a>
+         </li>
+         <li>
+            <span tabindex="0">
+            Students Corner
+            </span>
+         </li>        
+         <li>
+            <span tabindex="0">
+            TEDx
+            </span>
+         </li>
+      </ul>
+   </div>
+</div>
+<section class="withsidebar-wrap ptb-60">
+   <div class="container">
+      <div class="row">
+         <div class="col master-class">
+            <div class="innerpagecontent master-class">
+               <!-- Heading section Start -->
+               <h3 tabindex="0">
+                  <span tabindex="0">
+                  TEDx
+                  </span>
+               </h3>
+               <!-- Heading section End -->
+              
+                <p><b>What's TEDx?</b></p>
+                <p>Under the motto "ideas worth spreading," TED Conferences, LLC, an American-Canadian non-profit media organization, uploads international presentations online for free distribution.</p>
+
+                <p>A TEDx event is a community gathering where live acts and TED-like speeches are shared. Each TEDx community plans and manages its events autonomously.</p>
+
+                <p><b>About TEDxIIMKashipur</b></p>
+
+                <p>We are a team of student volunteers who put together a one-of-a-kind event. We aim to bring together thinkers, specialists, decision-makers, and change agents who alter mindsets and bring about systematic change in people's lives. Every year, a weekend of carefully curated Talks that spark conversation in our community and beyond.&nbsp;&nbsp;</p>
+
+                <p>For the past six years, we have organized our flagship Annual TEDxIIMKashipur Conference, sharing with our community the insights and experiences of corporate executives, academicians, engineers, politicians, and many others.&nbsp;</p>
+
+                <p>Our Talks are published on the popular TEDx YouTube channel for free for national and international audiences. Additionally, we provide access to the Talks on this website too.</p>
+
+                <p><b>The theme for 2022: Time and Time Again!</b></p>
+
+                <p>TEDxIIMkashipur is back with another thought-provoking edition of TED,&nbsp;<strong>Time and Time Again</strong>.</p>
+
+                <p><em><b>Time and time again</b></em>&nbsp;refers to something that occurs quite often; it could reflect experiences, moments, etc. Like history often repeats itself, a moment can neither buried nor shunned. It repeats itself and comes back to us. To this line, we bring you yet another exuberant lineup of exuberant speakers to share their excerpts with us!</p>
+                                  
+
+               <!-- Chairpersons -->
+               <h5 tabindex="0">
+                  <span tabindex="0">  Chairperson </span>
+               </h5>
+               <div class="row mt-4 mb-5">
+                  <div class="col-md-2">
+                     <div class="top text-left mt-0">
+                        <div class="profile-img img-fac">
+                           <img src="{{ asset('uploads/organisation/167903379419.jpg') }}" alt="ABHRADEEP MAITI" loading="lazy" class="mb-0">
+                           <div class="d-flex justify-content-center">
+                              <div class="top-text mb-0 p-relative"> ABHRADEEP MAITI </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-md-9">
+                     <div class="designation">
+                        <h4 tabindex="0">Assistant Professor & Area Chairperson </h4>
+                        <h6 tabindex="0">ABHRADEEP MAITI </h6>
+                        <h6 tabindex="0">+91-7900444090 </h6>
+                        <h6 tabindex="0">abhradeep[dot]maiti[at]iimkashipur[dot]ac[dot]in </h6>
+                     </div>
+                  </div>
+               </div>
+               <!-- Photo Gallery Section Start -->
+               <h5 tabindex="0">
+                  <span tabindex="0">Members</span>
+               </h5>
+               <div class="excellence-wrap back-img Activities gallery-member img-gallery mb-3 mt-4">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-md-12 p-0">
+                           <div class="excellence-gallery partnership-img">
+                              <div class="row masonry-grid">
+                                 <div class="col-md-2 col-lg-2">
+                                    <div class="d-flex flex-column h-100">
+                                       <a href="javascript:void();" class="">
+                                          <div class="thumbnail p-relative text-center">
+                                             <img src="{{ asset('page/tdex/member/ChandradityaDas_TEDx.jpg') }}" alt="gallery-img" class="img-fluid" loading="lazy">
+                                             <div class="top-text">Chandraditya Das</div>
+                                          </div>
+                                       </a>
+                                    </div>
+                                 </div>
+                                 <div class="col-md-2 col-lg-2">
+                                    <div class="d-flex flex-column h-100">
+                                       <a href="javascript:void();" class="">
+                                          <div class="thumbnail p-relative text-center">
+                                             <img src="{{ asset('page/tdex/member/PushkarTandon_TEDx.jpg') }}" alt="gallery-img" class="img-fluid" loading="lazy">
+                                             <div class="top-text">Pushkar Tandon
+                                             </div>
+                                          </div>
+                                       </a>
+                                    </div>
+                                 </div>
+                                 <div class="col-md-2 col-lg-2">
+                                    <div class="d-flex flex-column h-100">
+                                       <a href="javascript:void();" class="">
+                                          <div class="thumbnail p-relative text-center">
+                                             <img src="{{ asset('page/tdex/member/RajRajeshwariKayan.jpg') }}" alt="gallery-img" class="img-fluid" loading="lazy">
+                                             <div class="top-text">Raj Rajeshwari Kayan
+                                             </div>
+                                          </div>
+                                       </a>
+                                    </div>
+                                 </div>
+                                 <div class="col-md-2 col-lg-2">
+                                    <div class="d-flex flex-column h-100">
+                                       <a href="javascript:void();" class="">
+                                          <div class="thumbnail p-relative text-center">
+                                             <img src="{{ asset('page/tdex/member/Sakshi_TEDx.jpg') }}" alt="gallery-img" class="img-fluid" loading="lazy">
+                                             <div class="top-text">Sakshi
+                                             </div>
+                                          </div>
+                                       </a>
+                                    </div>
+                                 </div>
+                                 <div class="col-md-2 col-lg-2">
+                                    <div class="d-flex flex-column h-100">
+                                       <a href="javascript:void();" class="">
+                                          <div class="thumbnail p-relative text-center">
+                                             <img src="{{ asset('page/tdex/member/SharayuBankar_TEDx.jpg') }}" alt="gallery-img" class="img-fluid" loading="lazy">
+                                             <div class="top-text">Sharayu Bankar
+                                             </div>
+                                          </div>
+                                       </a>
+                                    </div>
+                                 </div>
+                                 <div class="col-md-2 col-lg-2">
+                                    <div class="d-flex flex-column h-100">
+                                       <a href="javascript:void();" class="">
+                                          <div class="thumbnail p-relative text-center">
+                                             <img src="{{ asset('page/tdex/member/ShikhaSinghal.png') }}" alt="gallery-img" class="img-fluid" loading="lazy">
+                                             <div class="top-text">Shikha Singhal
+                                             </div>
+                                          </div>
+                                       </a>
+                                    </div>
+                                 </div>
+                                 <div class="col-md-2 col-lg-2">
+                                    <div class="d-flex flex-column h-100">
+                                       <a href="javascript:void();" class="">
+                                          <div class="thumbnail p-relative text-center">
+                                             <img src="{{ asset('page/tdex/member/SusheelKumarDheeravath_TEDx.jpg') }}" alt="gallery-img" class="img-fluid" loading="lazy">
+                                             <div class="top-text">Susheel Kumar Dheeravath
+                                             </div>
+                                          </div>
+                                       </a>
+                                    </div>
+                                 </div>
+
+                                 <div class="col-md-2 col-lg-2">
+                                    <div class="d-flex flex-column h-100">
+                                       <a href="javascript:void();" class="">
+                                          <div class="thumbnail p-relative text-center">
+                                             <img src="{{ asset('page/tdex/member/Aryan_TEDx.jpg') }}" alt="gallery-img" class="img-fluid" loading="lazy">
+                                             <div class="top-text">Aryan
+                                             </div>
+                                          </div>
+                                       </a>
+                                    </div>
+                                 </div>
+                                
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- Event section start  -->
+               <h5 tabindex="0">
+                  <span tabindex="0">Events</span>
+               </h5>
+               <div class="excellence-wrap event-text mb-3 mt-4">
+                  <div class="container p-0">
+                     <div class="row">
+                        <!-- Multiple Image Popup -->
+                        <div id="gallery-1" class="hidden">
+                           <a href="{{ asset('page/tdex/event/e1.jpg') }}"></a>
+                           <a href="{{ asset('page/tdex/event/e2.jpg') }}"></a>
+                           <a href="{{ asset('page/tdex/event/e3.jpg') }}"></a>
+                           <a href="{{ asset('page/tdex/event/e4.jpg') }}"></a>
+                           <a href="{{ asset('page/tdex/event/e5.jpg') }}"></a>
+                           <a href="{{ asset('page/tdex/event/e6.jpg') }}"></a>
+                           <a href="{{ asset('page/tdex/event/e7.jpg') }}"></a>                                                 
+                        </div>
+                        <div class="col-md-3">
+                           <div class="multi-image-popup p-relative">
+                              <a href="#gallery-1" class="btn-gallery multi-card">
+                                 <div class="card1"></div>
+                                 <div class="card3"></div>
+                                 <div class="card4"></div>
+                                 <div class="card5"></div>
+                                 <img src="{{ asset('page/tdex/event/e1.jpg') }}" loading="lazy">
+                              </a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- Event section end -->
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
 
 
+     <script>
+        $(document).ready(function() {
+            $('.image-link').magnificPopup({
+                type: 'image',
+                mainClass: 'mfp-with-zoom',
+                gallery: {
+                    enabled: true
+                },
 
+                zoom: {
+                    enabled: true,
+                    duration: 300, // duration of the effect, in milliseconds
+                    easing: 'ease-in-out', // CSS transition easing function
+                    opener: function(openerElement) {
+                        return openerElement.is('img') ? openerElement : openerElement.find('img');
+                    }
+                }
+            });
+
+
+            $('a.btn-gallery').on('click', function(event) {
+                event.preventDefault();
+                var gallery = $(this).attr('href');
+                $(gallery).magnificPopup({
+            delegate: 'a',
+                    type:'image',
+                    gallery: {
+                        enabled: true
+                    }
+                }).magnificPopup('open');
+            });
+            });
+    </script>
 
 
 @endsection
