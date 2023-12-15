6fdf6ba01e26614871ea24ce4fb808a6d1baee9b
neeraj7355 <neeraj.chaursiya@echttech.com>
2023-12-15 12:49:20 +0530

    fixed marquee

--- resources/views/front/Layouts/home_sections/Student.blade.php
+++ resources/views/front/Layouts/home_sections/Student.blade.php
@@ -153,84 +153,66 @@
                    <div class="event-card aos-init aos-animate" data-aos="fade-up" data-aos-duration="3000">
                        <h2 class="heading-black text-start" tabindex="0">@lang('common.Notice-Board')</h2>
                        <div class="myslider">
+                           <!-- ******************************************** -->
 
+                           <!-- ******************************************** -->
+                           <div class="marquee marquee-vertical" data-speed="50">
+                               <div class="marquee-wrapper event-body   ">
+                                   <div class="marquee-content">
+                                       @if (count(Getheading($M->id)) > 0)
+                                       @foreach (Getheading($M->id) as $key => $Ms)
+                                       {{-- {{ Getarchivedata(now()->format('Y-m-d'), $Ms->archive_date) != 'True' }}
+                                       --}}
+                                       @if (Getarchivedata(now()->format('Y-m-d'), $Ms->archive_date) != 'True')
+                                       <div class="event-content notice-body">
+                                           <a @if ($Ms->external == 'yes') @if (GetLang() == 'en') onclick="return
+                                               confirm('This link will take you to an external web site.')" @else
+                                               onclick="return confirm('यह लिंक आपको एक बाहरी वेब साइट पर ले
+                                               जाएगा।')"
+                                               @endif
+                                               href="{{ $Ms->url }}"
+                                               @elseif($Ms->external == 'no')
+                                               href="{{ url($Ms->url) }}" @endif
+                                               class="red-link">
+                                               <h3 class="title" tabindex="0">
+                                                   @if (GetLang() == 'en')
+                                                   {{ $Ms->short }}
+                                                   @else
+                                                   {{ $Ms->short_h }} @endif
+                                               </h3>
 
-
-                           <div class="marquee">
-                               <div class="event-body item  marquee--inner pt-0">
-
-                                   @if (count(Getheading($M->id)) > 0)
-                                   @foreach (Getheading($M->id) as $key => $Ms)
-                                   {{-- {{ Getarchivedata(now()->format('Y-m-d'), $Ms->archive_date) != 'True' }} --}}
-                                   @if (Getarchivedata(now()->format('Y-m-d'), $Ms->archive_date) != 'True')
-
-                                   <div class="marquee marquee-vertical" data-speed="50">
-                                       <div class="marquee-wrapper">
-
-                                           <div class="marquee-content">
-                                               <div class="event-content notice-body" >
+                                               <div class="btn-wrap">
                                                    <a @if ($Ms->external == 'yes') @if (GetLang() == 'en')
-                                                       onclick="return confirm('This link will take you to an external
-                                                       web site.')" @else onclick="return confirm('यह लिंक आपको एक बाहरी
-                                                       वेब साइट पर ले जाएगा।')" @endif
+                                                       onclick="return
+                                                       confirm('This link will take you to an external web site.')"
+                                                       @else
+                                                       onclick="return confirm('यह लिंक आपको एक बाहरी वेब साइट पर ले
+                                                       जाएगा।')"
+                                                       @endif
                                                        href="{{ $Ms->url }}"
                                                        @elseif($Ms->external == 'no')
                                                        href="{{ url($Ms->url) }}" @endif
                                                        class="red-link">
-                                                       <h3 class="title" tabindex="0">
-                                                           @if (GetLang() == 'en')
-                                                           {{ $Ms->short }}
-                                                           @else
-                                                           {{ $Ms->short_h }} @endif
-
-                                                       </h3>
-
-                                                       <div class="btn-wrap ">
-                                                           <a @if ($Ms->external == 'yes') @if (GetLang() == 'en')
-                                                               onclick="return confirm('This link will take you to an
-                                                               external web site.')" @else onclick="return confirm('यह
-                                                               लिंक आपको एक बाहरी वेब साइट पर ले जाएगा।')" @endif
-                                                               href="{{ $Ms->url }}"
-                                                               @elseif($Ms->external == 'no')
-                                                               href="{{ url($Ms->url) }}" @endif
-                                                               class="red-link">
-                                                               @lang('common.READ_MORE') <img
-                                                                   src="assets/images/read-more.svg" alt="read-more"
-                                                                   class="img-fluid">
-                                                           </a>
-                                                       </div>
+                                                       @lang('common.READ_MORE') <img src="assets/images/read-more.svg"
+                                                           alt="read-more" class="img-fluid">
                                                    </a>
                                                </div>
-                                           </div>
+                                           </a>
                                        </div>
+                                       @endif
+                                       @endforeach
+                                       @endif
+
+
+
+
+
+
                                    </div>
-                                   @endif
-                                   @endforeach
-                                   @endif
-
                                </div>
-
-
-                               <!-- <div class="event-body item">
-                            <div class="event-image">
-                                <img src="{{ asset('uploads/header_top/167776088848.png') }}" title="Notice Board" alt="Notice Board" class="img-fluid" loading="lazy">
-                            </div>
-                            <div class="event-content">
-                                <div class="d-flex">
-                                    <p class="date"><i class="fa fa-calendar" alt="calendar"></i> 2023-03-02 18:45:11</p>
-                                </div>
-                                <h3 class="title">IIM KASHIPUR CONGRATULATES ON CLEARING CFA LEVEL 2</h3>
-
-                                <div class="btn-wrap">
-                                    <a href="#" class="red-link">
-                                        READ MORE 2 <img src="assets/images/read-more.svg" alt="read-more" class="img-fluid" loading="lazy">
-                                    </a>
-
-                                </div>
-
-                            </div>
-                        </div> -->
                            </div>
+                           <!-- ************************************************* -->
+
 
                        </div>
                    </div>
@@ -270,3 +252,4 @@ console.log("marquee is running");
 
 // });
    </script>
+
